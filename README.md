771e94b8c94ddf2acda49688299797a1d40fa401
Guillaume Ferron <guillaume.ferron@gmail.com>
2020-04-26 17:12:23 +0200

    add story building mecanism

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ascii-kitty
-Little ascii kitty that can speak
+Little ascii kitty that can speak and move
 
              ^  ^  hello
             (ù.ù )___(
@@ -8,3 +8,67 @@ Little ascii kitty that can speak
              ^  ^  world!
             ( ô.ô)___(
              //||-//||
+
+
+## Add a state
+
+```
+ls -l states/
+... state1.yaml
+... state2.yaml
+... state3.yaml
+```
+
+### A state is a single snapshot that describe the cat
+```
+Example:
+
+state:
+    ears : '             ^   ^'
+    face : '            (ù.ù )___('
+    paws : '             ||\\-||\\'
+
+```
+
+### Add chit-chat in a state
+```
+Example:
+
+state:
+    sky : '                     hello world!'
+    ears : '             ^   ^  /  '
+    face : '            (ù.ù )___( '
+    paws : '             ||\\-||\\ '
+
+```
+
+## Build Stories
+
+```
+ls -l stories/
+... story1.yaml
+... story2.yaml
+... story3.yaml
+```
+
+## A story is a yaml file that describes a sequence of states
+
+```
+Example:
+
+- state: "state1"
+  duration: 1
+
+- state: "state2"
+  duration: 1
+
+- state: 'state2'
+  duration: 1
+
+- state: 'state3'
+  duration: 1
+
+- state: 'state2'
+  duration: 1
+
+```
--- ascii_kitty.py
+++ ascii_kitty.py
@@ -1,20 +1,47 @@
 import time
+import yaml
+
+story = 'basic'
+states = {
+        'std' : yaml.load(open('states/std.yaml', 'r')),
+        'walk1' : yaml.load(open('states/walk1.yaml', 'r')),
+        'walk2' : yaml.load(open('states/walk2.yaml', 'r')),
+        'std_speak' : yaml.load(open('states/std_speak.yaml', 'r')),
+        'surprised' : yaml.load(open('states/surprised.yaml', 'r'))
+        }
+
+stream = open('stories/'+story+'.yaml', 'r')
+story = yaml.load(stream)
 
 msg1 = "world!"
 msg2= "hello"
 duration = 1
 max = 100
 
-chat1 = f"\n             ^  ^  {msg1}\n            ( ô.ô)___(\n             //||-//||"
-chat2 = f"\n             ^   ^  {msg2}\n            (ù.ù )___(\n             //||-//||"
+def build_story(story, states):
 
-chat = chat1
+    movie = []
 
-for a in range(1,max):
-    print(chr(27) + "[2J")
-    if chat == chat1:
-        chat = chat2
+    for event in story:
+        for duration in range(event['duration']):
+            for key in states[event['state']]['state']:
+                if 'env' in states[event['state']]:
+                    states[event['state']]['state'][key] = states[event['state']]['state'][key].format(**states[event['state']]['env'])
+            movie.append(states[event['state']])
+    return movie 
+    
+
+movie = build_story(story, states)
+
+cursor = -1
+for a in range(1,len(movie) * max):
+
+    if cursor >= len(movie) - 1:
+        cursor = 0
     else:
-        chat = chat1
-    print(chat)
-    time.sleep(duration)
+        cursor = cursor + 1
+
+    print(chr(27) + "[2J")
+    for key in movie[cursor]['state']:
+        print(movie[cursor]['state'][key])
+    time.sleep(1)
